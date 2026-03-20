7737a1a880cdb86ebe2888a2339c8c14deba9cde
Illia Murashka <lolens56@gmail.com>
2026-03-20 13:11:49 +0300

    add license

--- common/src/main/java/io/github/lolens/showcaser/command/ServerCommands.java
+++ common/src/main/java/io/github/lolens/showcaser/command/ServerCommands.java
@@ -1,3 +1,23 @@
+/*
+ * Showcaser - <https://github.com/Lolens/showcaser>
+ * Copyright (C) 2026-present Lolens <https://github.com/Lolens>
+ *
+ * This file is part of Showcaser.
+ *
+ * Showcaser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * Showcaser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along Showcaser.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.github.lolens.showcaser.command;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
